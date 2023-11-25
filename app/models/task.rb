5d717d94c2ba4378f2cab25b655d49e52759ff45
issei-yoshi <iyoshi.engineer@gmail.com>
2023-11-25 12:28:47 +0900

    use dirty method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  enum :status, %w[initial in_progress completed]
+  enum :status, %w[initial in_progress completed], prefix: true
+
+  after_save_commit :update_user_level!
+
+  def update_user_level!
+    return unless saved_change_to_status?
+
+    if status_completed?
+      user.level += 1
+      user.save!
+    end
+  end
 end
